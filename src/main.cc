d07f8e82726da1bc3b5342ec4ef383979550a8eb
scjrobertson <robertsonscj@gmail.com>
2016-09-27 11:38:15 +0200

    Minor changes

---
+++ src/main.cc
@@ -0,0 +1,34 @@
+/*************************************************************************
+ *  Compilation:  
+ *  Execution:    
+ *  Dependencies:
+ *
+ * Main app, runs small examples for now.
+ *************************************************************************/
+
+#include <iostream>
+#include <string>
+
+#include "mrandom.hpp"
+#include "discretetable.hpp"
+#include "clustergraph.hpp"
+#include "lbp_cg.hpp"
+#include "lbu_cg.hpp"
+#include "v2vtransform.hpp"
+#include "combinations.hpp"
+
+using namespace emdw;
+
+/**
+ * Main app, runs small examples for now.
+ *
+ * @author SCJ Robertson
+ * @since 0.9.1
+ */
+int main(int, char *argv[]) {
+	typedef unsigned short T;
+	typedef DiscreteTable<T> DT;
+	typedef std::vector<T> DASS;
+
+	std::cout << "Not broken yet." << std::endl;
+}
